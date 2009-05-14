131a4477faae6bb97dbda4c98ebd087d1b4f5fd6
Agon Cilku <agoncilku@gmail.com>
2009-05-14 08:38:49 +0000

    Initial Implementation

--- src/unisono/mmplugins/node_ressources.py
+++ src/unisono/mmplugins/node_ressources.py
@@ -27,3 +27,60 @@ file_name
  along with UNISONO.  If not, see <http://www.gnu.org/licenses/>.
  
 '''
+import threading, logging, re, string, sys
+
+class resourcereader:
+    '''
+    generic template for all M&Ms
+    '''
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    name = "wlanreader"
+    
+    def __init__(self,name):
+        '''
+        init the M&M and start the thread
+        '''
+        self.name = name
+        mm_thread = threading.Thread(target=self.run)
+        # Exit the server thread when the main thread terminates
+        mm_thread.setDaemon(True)
+        mm_thread.start()
+        self.logger.info("M&M %s loop running in thread: %s", self.name, mm_thread.name)
+        
+    def measure(self):
+            cpu = open("/proc/cpuinfo" , "r")
+            info = {}
+            for cpuinfo in cpu:
+                cpuinfo = cpuinfo.rstrip('\n')
+                if re.match("(.*)model name(.*)", cpuinfo):
+                    
+                    print(cpuinfo)
+                if re.match("(.*)cpu MHz(.*)", cpuinfo):
+                    print(cpuinfo)
+                if re.match("(.*)cache size(.*)", cpuinfo):
+                    print(cpuinfo)
+                if re.match("(.*)cpu cores(.*)", cpuinfo):
+                    print(cpuinfo)
+                    print('\n')
+            ram = open("/proc/meminfo", "r")
+            for meminfo in ram:
+                meminfo = meminfo.rstrip('\n')
+                if re.match("(.*)MemTotal(.*)", meminfo):
+                    print(meminfo)
+                if re.match("(.*)MemFree(.*)", meminfo):
+                    print(meminfo)
+                if re.match("(.*)SwapTotal(.*)", meminfo):
+                    print(meminfo)
+                if re.match("(.*)SwapFree(.*)", meminfo):
+                    print(meminfo)
+                    print('\n')
+            os = open("/proc/version")
+            for kernel in os:
+                if re.match("(.*)(.*)", kernel):
+                    print(kernel)
+                    
+    def provideDataItems(self):
+        pass
+    def provideMMname(self):
+        pass
