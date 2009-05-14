921c3cd76aa045f3693c9940c3d27c1f5b8730e1
Dirk Haage <haage@net.in.tum.de>
2009-05-14 11:50:21 +0000

    the request struct is now prepared but it might not work this way.

--- src/unisono/mmplugins/pathmtu.py
+++ src/unisono/mmplugins/pathmtu.py
@@ -61,5 +61,13 @@ class PathMTU(mmtemplates.MMcTemplate):
 
     def prepare_request(self, req):
         creqstruct = PathMTURequest()
-        # TODO prepare the struct
+        # FIX prepare the struct -> string to c_char_p??!
+        if 'identifier1' in req.keys():
+            creqstruct.identifier1 = req['identifier1']
+        else:
+            creqstruct.identifier1 = ''
+        if 'identifier2' in req.keys():
+            creqstruct.identifier2 = req['identifier2']
+        else:
+            creqstruct.identifier2 = ''
         return creqstruct
