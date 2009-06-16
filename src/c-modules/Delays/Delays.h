b9fd36edc78c08fe3925d806e5b96d7bd88f40fd
Dirk Haage <haage@net.in.tum.de>
2009-06-16 10:11:46 +0000

    updated license headers, fixes #127

--- src/c-modules/Delays/Delays.h
+++ src/c-modules/Delays/Delays.h
@@ -8,22 +8,23 @@
  *  $LastChangedDate$
  *         $Revision$
  *
- *  (C) 2008-2009 by Computer Networks and Internet, University of Tuebingen
+ * (C) 2008-2009 by Computer Networks and Internet, University of Tuebingen
  *
- *  This file is part of CLIO Cross-Layer Information Service for Overlays.
+ * This file is part of UNISONO Unified Information Service for Overlay
+ * Network Optimization.
  *
- *  CLIO is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 2 of the License, or
- *  (at your option) any later version.
+ * UNISONO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
  *
- *  CLIO is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * UNISONO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with CLIO.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with UNISONO.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef CDELAY_H_
