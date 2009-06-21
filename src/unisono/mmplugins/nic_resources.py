1c7040a150dcdec02ff9a2443ae966f8484f6f06
Agon Cilku <agoncilku@gmail.com>
2009-06-21 10:34:47 +0000

    fixed #136 Informations are passed to the debugger.

--- src/unisono/mmplugins/nic_resources.py
+++ src/unisono/mmplugins/nic_resources.py
@@ -1,5 +1,5 @@
 '''
-file_name
+nic_resources.py
 
  Created on: May 14, 2009
  Authors: dh
@@ -46,6 +46,7 @@ class NicReader(mmtemplates.MMTemplate):
         '''
         super().__init__(*args)
         self.dataitems = [
+                          'INTERFACE',
                           'INTERFACE_TYPE',
                           'INTERFACE_CAPACITY_RX',
                           'INTERFACE_CAPACITY_TX',    
@@ -85,66 +86,69 @@ class NicReader(mmtemplates.MMTemplate):
                 self.request['error'] = interface
 
 
-        
         self.request['INTERFACE_TYPE'] = popen("ifconfig " + interface + " | grep encap | perl -ple '($_) = /encap:([^ ]+)/'").read()
-        print("Interface Type: ****************************************" + self.request['INTERFACE_TYPE'])
-        
-        
-        #TODO im system monitor sourcecode die quelle herausfinden
+        #TODO find information source in Systemmonitor
         self.request['INTERFACE_CAPACITY_RX'] = "cant find"
-        print("Interface Capacity RX: " + self.request['INTERFACE_CAPACITY_RX'])
-        #TODO im system monitor sourcecode die quelle herausfinden
+        #TODO find information source in Systemmonitor
         self.request['INTERFACE_CAPACITY_TX'] = "cant find"
-        print("Interface Capacity TX: " + self.request['INTERFACE_CAPACITY_TX'])
-        
-        
-        self.request['INTERFACE_MAC'] = popen("ifconfig " + interface + " | grep HWaddr | perl -ple '($_) = /HWaddr\s(.+)\s/'").read()
-        print("Interface Mac: **************************************** " + self.request['INTERFACE_MAC'])
-        
+        self.request['INTERFACE_MAC'] = popen("ifconfig " + interface + " | grep HWaddr | perl -ple '($_) = /HWaddr\s(.+)\s/'").read()     
         self.request['INTERFACE_MTU'] = popen("ifconfig " + interface + " | grep MTU | perl -ple '($_) = /MTU:([^ ]+)/'").read()
-        print("Interface MTU: " + self.request['INTERFACE_MTU'])
-        
-        #TODO im system monitor sourcecode die quelle herausfinden
+        #TODO find information source in Systemmonitor
         self.request['USED_BANDWIDTH_RX'] = "cant find"
-        print("Interface Bandwidth RX: " + self.request['USED_BANDWIDTH_RX'])
-        #TODO im system monitor sourcecode die quelle herausfinden
+        #TODO find information source in Systemmonitor
         self.request['USED_BANDWIDTH_TX'] = "cant find"
-        print("Interface Bandwidth TX: " + self.request['USED_BANDWIDTH_TX'])
+
+
+
+        #Interface Information
+        self.logger.debug('The result is: %s', self.request['INTERFACE_TYPE'])
+        self.logger.debug('The result is: %s', self.request['INTERFACE_CAPACITY_RX'])
+        self.logger.debug('The result is: %s', self.request['INTERFACE_CAPACITY_TX'])
+        self.logger.debug('The result is: %s', self.request['INTERFACE_MAC'])
+        self.logger.debug('The result is: %s', self.request['INTERFACE_MTU'])
+        self.logger.debug('The result is: %s', self.request['USED_BANDWIDTH_RX'])
+        self.logger.debug('The result is: %s', self.request['USED_BANDWIDTH_TX'])
+            
+    
         
-        # in diesem bereich wird was wlan behandelt 
-        # im fall das es ein wlan ist dann werden 
-        # die einzelnen felder der dictionary mit 
-        # der ausgabe von iwconfig aufgefült
+        self.request['error'] = 0
+        self.request['errortext'] = 'Measurement successful'
+
+def WifiReader(mmtemplate):
+    pass
+        
+        
+        
+
+    def measure(self):
+        ipaddress = self.request['identifier1']
+        interfacelist = popen('dir /proc/net/dev_snmp6/').read().split()
+        self.logger.debug(interfacelist)
+        interface = "No wireless interface with this IP"
+        intinfo=''
+        for i in interfacelist:
+            intinfo = popen('iwconfig ' + i).read()
+            if ipaddress in intinfo:
+                interface = i
+                break
+            else: 
+                self.request['error'] = interface   
+        
+        # in diesem bereich wird was wlan behandelt im fall das es ein wlan ist dann werden 
+        # die einzelnen felder der dictionary mit der ausgabe von iwconfig aufgefült
         wlaninfo = popen('iwconfig ' + interface).read()
         if "no wireless extensions" not in wlaninfo:
             self.request['WLAN_ESSID']          = popen("iwconfig " + interface + " | grep ESSID | perl -ple '($_) = /ESSID:([^ ]+)/'").read()
-            # nur zur fehler kontrolle
-            print("Wlan Essid ****************************************:" + self.request['WLAN_ESSID'])
-            
             self.request['WLAN_MODE']           = popen("iwconfig " + interface + " | grep Mode | perl -ple '($_) = /Mode:([^ ]+)/'").read()
-            print("Wlan Mode ****************************************: " + self.request['WLAN_MODE'])
-            
-            self.request['WLAN_AP_MAC']         = popen("iwconfig " + interface + " | grep Point | perl -ple '($_) = /Point:\s([^ ]+)/'").read()
-            print("Wlan AP Mac ****************************************: " + self.request['WLAN_AP_MAC'])
-            
+            self.request['WLAN_AP_MAC']         = popen("iwconfig " + interface + " | grep Point | perl -ple '($_) = /Point:\s([^ ]+)/'").read(        
             self.request['WLAN_LINK']           = popen("iwconfig " + interface + " | grep Quality | perl -ple '($_) = /Quality=([^ ]+)/'").read()
-            print("Wlan Link ****************************************: " + self.request['WLAN_LINK'])
-            
             self.request['WLAN_SIGNAL']         = popen("iwconfig " + interface + " | grep Signal | perl -ple '($_) = /Signal level:([^ ]+)/'").read()
-            print("Wlan Signal ****************************************: " + self.request['WLAN_SIGNAL'])
-            
             self.request['WLAN_NOISE']          = popen("iwconfig " + interface + " | grep Noise | perl -ple '($_) = /Noise level=([^ ]+)/'").read()
-            print("Wlan Noise ****************************************: " + self.request['WLAN_NOISE'])
-            
-            #muss die noch richtig ausgerechnet werden
-            #self.request['WLAN_SIGNOISE_RATIO'] = (self.request['WLAN_SIGNAL'] / self.request['WLAN_NOISE'])
-            #print("Wlan Essid: " + self.request['WLAN_ESSID'])
-            
+            #TODO add correct calculation 
+            self.request['WLAN_SIGNOISE_RATIO'] = (self.request['WLAN_SIGNAL'] / self.request['WLAN_NOISE'])         
             self.request['WLAN_CHANNEL']        = popen("iwlist " + interface + " channel | grep Frequency | perl -ple '($_) = /Channel\s([^ ]+)/'").read()
-            print("Wlan Channel ****************************************: " + self.request['WLAN_CHANNEL'])
-            
             self.request['WLAN_FREQUENCY']      = popen("iwconfig " + interface + " | grep Frequency | perl -ple '($_) = /Frequency:([^ ]+)/'").read()
-            print("Wlan Frequenz ****************************************: " + self.request['WLAN_FREQUENCY'])
+            
         else:
             # im es sich nicht um ein wlan handel dan 
             # werden die felder mit "this is not a wireless
@@ -159,11 +163,19 @@ class NicReader(mmtemplates.MMTemplate):
             self.request['WLAN_CHANNEL']        = "this is not a wireless interface"
             self.request['WLAN_FREQUENCY']      = "this is not a wireless interface"
             
-    
-        
-        self.request['error'] = 0
-        self.request['errortext'] = 'Measurement successful'
 
-def WifiReader(mmtemplate):
-    pass
+
+        #Wireless information
+        self.logger.debug('The result is: %s', self.request['WLAN_ESSID'])
+        self.logger.debug('The result is: %s', self.request['WLAN_MODE'])
+        self.logger.debug('The result is: %s', self.request['WLAN_AP_MAC'])
+        self.logger.debug('The result is: %s', self.request['WLAN_LINK'])
+        self.logger.debug('The result is: %s', self.request['WLAN_SIGNAL'])
+        self.logger.debug('The result is: %s', self.request['WLAN_NOISE'])
+        self.logger.debug('The result is: %s', self.request['WLAN_ESSID'])
+        self.logger.debug('The result is: %s', self.request['WLAN_FREQUENCY'])
+
+
+
+
         
